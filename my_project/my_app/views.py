881a43bf463254a9cf3534145d950fe5fbf5f1aa
blandskron <bastianlandskronfreelancer@gmail.com>
2024-12-14 19:45:38 -0300

    update

---
+++ my_project/my_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import User, Product
+
+def list_users(request):
+    users = User.objects.all()
+    return render(request, 'list_users.html', {'users': users})
+
+def products_in_stock(request):
+    products = Product.objects.filter(stock__gt=0)
+    return render(request, 'products.html', {'products': products})
